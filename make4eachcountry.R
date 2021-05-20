11451811eacde5b3c1710bec106ec2a7951cd3b3
olanderb <billyolander@gmail.com>
2021-05-20 09:06:15 +0000

    Create make4eachcountry.R

---
+++ make4eachcountry.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(readxl)
+
+cadre_harmonise_caf_ipc <- read_excel("cadre_harmonise_caf_ipc.xlsx")
+
+reports <- tibble(
+  adm0_name = unique(cadre_harmonise_caf_ipc$adm0_name),
+  filename = stringr::str_c("CH-report-", adm0_name, ".html"),
+  params = purrr::map(adm0_name, ~ list(country = .))
+)
+reports
+
+reports %>% 
+  select(output_file = filename, params) %>% 
+  purrr::pwalk(rmarkdown::render, input = "parameterized_CHdashboard.Rmd")
+
